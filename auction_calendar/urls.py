b58cced093be7895cc82cbdc0e51185b2ef9e4fe
Obaidul Hossain <142455508+obaidulhossain@users.noreply.github.com>
2024-12-22 10:45:37 +0600

    20241222-1045AM

--- auction_calendar/urls.py
+++ auction_calendar/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
+    path('dashboard1/', views.dashboard1, name='dashboard1'),
     path('auction_calendar/', views.auction_calendar, name='auction_calendar'),
+    path('',views.client_access, name='client_access'),
+    path('dashboard/',views.dashboard, name='dashboard'),
 ]
 
 
