9102e499eed78144fe9b4cdd9ab18d2a5ea55e3d
Obaidul Hossain <142455508+obaidulhossain@users.noreply.github.com>
2024-12-23 11:15:04 +0600

    20241223-1114AM
    Master template updated

--- auction_calendar/urls.py
+++ auction_calendar/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('dashboard1/', views.dashboard1, name='dashboard1'),
     path('auction_calendar/', views.auction_calendar, name='auction_calendar'),
     path('',views.client_access, name='client_access'),
     path('dashboard/',views.dashboard, name='dashboard'),
--- auction_calendar/views.py
+++ auction_calendar/views.py
@@ -6,11 +6,6 @@ from django.template import loader
 def auction_calendar (request):
     template = loader.get_template('auction_calendar.html')
     return HttpResponse(template.render())
-
-def dashboard1 (request):
-    template = loader.get_template('dashboard1.html')
-    return HttpResponse(template.render())
-
 def client_access (request):
     template = loader.get_template('client_access.html')
     return HttpResponse(template.render())
