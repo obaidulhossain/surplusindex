1f2ca826938b555d43ee8a6766417c229a9bbe7c
Obaidul Hossain <142455508+obaidulhossain@users.noreply.github.com>
2025-01-10 00:43:23 +0600

    add

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -88,6 +88,9 @@ DATABASES = {
         'PASSWORD': 'Obaidul@11307',
         'HOST': '127.0.0.1',
         'PORT': '3306',
+                'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
     }
 }
 
