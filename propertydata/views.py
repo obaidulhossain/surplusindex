f73b092c30974554fc4825cc9392cdd1555dd834
Obaidul Hossain <142455508+obaidulhossain@users.noreply.github.com>
2025-02-05 10:14:21 +0600

    update

--- propertydata/views.py
+++ propertydata/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import AddPropertyForm
-from .models import Court_Record, Foreclosure, Status, User
+from .models import Court_Record, Foreclosure, Status
 from django.http import HttpResponse
 from django.core.paginator import Paginator
+from si_user.models import UserDetail
+from django.contrib import messages
 
 
 # Create your views here.
@@ -68,14 +70,55 @@ def myLeads(request):
 def addtoMyList(request):
     if request.method == "POST":
         selected_leads_ids = request.POST.getlist('selected_items')
+        lead_count = len(selected_leads_ids)
+        
+        # Get user credit details
+        user_details = UserDetail.objects.get(user=request.user)
+        free_credit = user_details.free_credit_balance
+        purchased_credit = user_details.purchased_credit_balance
 
-        for lead_id in selected_leads_ids:
-            Status.objects.create(lead_id=lead_id, client=request.user)
-            fcl = Foreclosure.objects.get(pk=lead_id)
-            fcl.purchased_by.add(request.user)
-        return redirect('leads')
+        # Check if user has enough credits
+        total_credit = free_credit + purchased_credit
+        if lead_count > total_credit:
+            messages.error(request, "Insufficient credits to add the selected leads.")
+            return redirect('leads')  # Redirect back to the leads page
+        else:
+            # Deduct credits
+            remaining_leads = lead_count
+
+            # Deduct from free credits first
+            if free_credit >= remaining_leads:
+                user_details.free_credit_balance -= remaining_leads
+                remaining_leads = 0
+                messages.info(request, str(lead_count) + ' Credits have been deducted from free credits')
+                messages.success(request,str(lead_count) + ' successfully added to My Leads. Visit My Leads Tab to explore lead details.')
+
+            else:
+                if free_credit >= 1:
+
+                    remaining_leads -= free_credit
+                    user_details.free_credit_balance = 0
+                    # Deduct the rest from purchased credits
+                    user_details.purchased_credit_balance -= remaining_leads
+                    messages.info(request, str(free_credit) + ' free credit and '+str(remaining_leads)+' purchased credit have beed deducted.') 
+                    messages.success(request,str(lead_count) + ' successfully added to My Leads. Visit My Leads Tab to explore lead details.')
+                else:
+                    user_details.purchased_credit_balance -= remaining_leads
+                    messages.info(request, str(remaining_leads)+' credits deducted from purchased credit balance.') 
+                    messages.success(request,str(lead_count) + ' successfully added to My Leads. Visit My Leads Tab to explore lead details.')
+            # Save updated credit balances
+            user_details.save()
+            user_details.update_total_credits()
+
+
+            for lead_id in selected_leads_ids:
+                Status.objects.create(lead_id=lead_id, client=request.user)
+                fcl = Foreclosure.objects.get(pk=lead_id)
+                fcl.purchased_by.add(request.user)
+            return redirect('leads')
     else:
         return HttpResponse("Invalid Request", status=400)
+
 
 
 def hidefromallLeads(request):
