408ae628fc8cbf0ac4d7f76c65d3dc872ec86721
Obaidul Hossain <142455508+obaidulhossain@users.noreply.github.com>
2025-05-13 13:31:52 +0600

    202505130131pm

--- si_user/admin.py
+++ si_user/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import *
 
 class UserDetailAdmin(admin.ModelAdmin):
-    list_display = ('user','phone', 'free_credit_balance','purchased_credit_balance','Total_credits')
+    list_display = ('user','user_type','phone', 'free_credit_balance','purchased_credit_balance','Total_credits')
 
 class UserPaymentAdmin(admin.ModelAdmin):
     list_display = ('stripe_customer_id','stripe_checkout_id', 'amount','number_of_leads','has_paid','currency')
