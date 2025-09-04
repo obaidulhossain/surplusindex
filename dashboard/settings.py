ec5dea840bee3e1b067afa39cf7d21d13774a266
Obaidul Hossain <142455508+obaidulhossain@users.noreply.github.com>
2025-09-04 13:08:24 +0600

    202509040108PM

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -178,7 +178,7 @@ EMAIL_HOST_USER = "contact@surplusindex.com"
 EMAIL_HOST_PASSWORD = "Obaidul@11307"
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
-DEFAULT_FROM_EMAIL = "contact@surplusindex.com"
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 # EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 # EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
 
