8c257695aacfb132aa387a5e09d799397769f4a7
Obaidul Hossain <142455508+obaidulhossain@users.noreply.github.com>
2025-09-14 15:52:24 +0600

    Update views.py

--- projects/views.py
+++ projects/views.py
@@ -35,7 +35,7 @@ def fclview(request):
         
     if selected_foreclosure:
         current_fcl_instance = get_object_or_404(Foreclosure, pk=selected_foreclosure)
-        event, created = foreclosure_Events.objects.get_or_create(state=current_fcl_instance.state, county=current_fcl_instance.county)
+        event, created = foreclosure_Events.objects.get_or_create(state=current_fcl_instance.state, county=current_fcl_instance.county, sale_type=current_fcl_instance.sale_type)
         all_prop = current_fcl_instance.property.all()
         all_plt = current_fcl_instance.plaintiff.all()
         all_def = current_fcl_instance.defendant.all()
