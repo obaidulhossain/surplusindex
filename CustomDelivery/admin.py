741b6ef03f6216e537267767a5a0fb0d67951775
Obaidul Hossain <142455508+obaidulhossain@users.noreply.github.com>
2025-10-20 19:31:43 +0600

    Update admin.py

--- CustomDelivery/admin.py
+++ CustomDelivery/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import*
 # Register your models here.
+@admin.register(CustomExportOptions)
+class CustomExportOptionsAdmin(admin.ModelAdmin):
+    list_display = ('client_name','client_email', 'number_delivery', 'next_delivery_date', 'delivery_type', 'contact_align', 'active')
+
+@admin.register(ExportLeadFilter)
+class ExportLeadFilterAdmin(admin.ModelAdmin):
+    list_display = ('filter_name','state', 'sale_type', 'sale_status', 'surplus_status')
