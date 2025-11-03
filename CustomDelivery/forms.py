dd358b3a1bf2e0795e959ea1db8b585a3f027abc
Obaidul Hossain <142455508+obaidulhossain@users.noreply.github.com>
2025-11-03 17:20:36 +0600

    Update forms.py

--- CustomDelivery/forms.py
+++ CustomDelivery/forms.py
@@ -13,10 +13,11 @@ class CustomExportOptionsForm(forms.ModelForm):
     class Meta:
         model = CustomExportOptions
         fields = [
-            "client_name", "client_email", "number_delivery", "next_delivery_date",
+            "client","client_name", "client_email", "number_delivery", "next_delivery_date",
             "delivery_type", "contact_align", "filter_option", "columns", "active"
         ]
         widgets = {
+            "client": forms.Select(attrs={"class": "form-select"}),
             "client_name": forms.TextInput(attrs={"class": "form-control"}),
             "client_email": forms.EmailInput(attrs={"class": "form-control"}),
             "number_delivery": forms.NumberInput(attrs={"class": "form-control"}),
@@ -24,6 +25,6 @@ class CustomExportOptionsForm(forms.ModelForm):
             "delivery_type": forms.Select(attrs={"class": "form-select"}),
             "contact_align": forms.Select(attrs={"class": "form-select"}),
             "filter_option": forms.SelectMultiple(attrs={"class": "form-select"}),
-            "columns": forms.Textarea(attrs={"class": "form-control", "rows": 2, "placeholder": '["id", "state", "sale_type", ...]'}),
+            "columns": forms.Textarea(attrs={"class": "form-control", "rows": 4, "placeholder": '["id", "state", "sale_type", ...]'}),
             "active": forms.CheckboxInput(attrs={"class": "form-check-input"}),
         }
