538434f9fc7feab8f5cc77f4317923e640b9d89d
Obaidul Hossain <142455508+obaidulhossain@users.noreply.github.com>
2025-12-02 02:16:21 +0600

    Update views.py

--- realestate_directory/views.py
+++ realestate_directory/views.py
@@ -344,7 +344,7 @@ def FilterEvents(request):
         Events = Events.filter(county__icontains=County)
     if SaleType:
         Events = Events.filter(sale_type__icontains=SaleType)
-    
+    Events = Events.order_by("county","sale_type")
 
     results = list(Events.values('id', 'state','county','population', 'sale_type', 'event_status','event_next','post_event_next'))
     return JsonResponse({'Events': results})
