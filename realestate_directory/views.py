e7c3a07cc2e16908ffc04af9b5ebba00044e8b77
Obaidul Hossain <142455508+obaidulhossain@users.noreply.github.com>
2025-12-02 02:31:36 +0600

    Update views.py

--- realestate_directory/views.py
+++ realestate_directory/views.py
@@ -36,6 +36,7 @@ def auctionCalendar(request):
         saledateFilter = request.GET.get('saledateFilter','')
     
     event_queryset = foreclosure_Events.objects.all()
+    event_queryset = event_queryset.order_by("state", "sale_type")
     states = event_queryset.values_list('state', flat=True).distinct()
     counties = event_queryset.values_list('county', flat=True).distinct()
     saletypes = event_queryset.values_list('sale_type', flat=True).distinct()
@@ -68,7 +69,7 @@ def auctionCalendar(request):
     elif saledateFilter == "upcoming":
         event_queryset = event_queryset.filter(event_next__gte=now().date())
 
-    event_queryset = event_queryset.order_by("county","sale_type")
+    # event_queryset = event_queryset.order_by("county","sale_type")
     # Paginate the results
     total_events = event_queryset.count()
     p = Paginator(event_queryset, 200)
