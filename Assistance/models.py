e644d3cbe11bb72a998b3c27664ad3f34e6f6130
Obaidul Hossain <142455508+obaidulhossain@users.noreply.github.com>
2026-01-22 18:04:02 +0600

    20250122 0603pm

--- Assistance/models.py
+++ Assistance/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class Timelogger(models.Model):
+    
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created')
+    changed_at = models.DateTimeField(auto_now=True, null=True, blank=True, verbose_name='Updated')
+    class Meta:
+        abstract = True
 # Create your models here.
